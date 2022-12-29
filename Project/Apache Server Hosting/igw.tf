ffca0800a42821b19470852965ef4d06b306966d
murulii <muruli800@gmail.com>
2022-12-29 14:48:25 +0530

    Delete Project directory

--- Project/Apache Server Hosting/igw.tf
+++
@@ -1,8 +0,0 @@
-resource "aws_internet_gateway" "gw" {
-  vpc_id = aws_vpc.Myvpc.id
-
-  tags = {
-    Name = "IGW"
-  }
-}
-
--- Project/Apache Server Hosting/provider.tf
+++
@@ -1,5 +0,0 @@
-provider "aws" {
-  region     = "us-east-2"
-  access_key = "AKIAS4F2QBKRRAYLODNF"
-  secret_key = "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"
-}
